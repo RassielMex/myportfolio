80a82f338e715b689ffc7d6d3982751e2917df1e
RassielMex <rhcp94.rm@gmail.com>
2022-08-28 11:48:21 -0500

    links and icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { Stack } from "@mui/system";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { Link } from "@mui/material";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Stack
+      justifyContent={"center"}
+      spacing={3}
+      direction={"row"}
+      marginTop={"3rem"}
+      marginBottom={"1rem"}
+    >
+      <Link
+        target={"_blank"}
+        rel="noopener"
+        color={"inherit"}
+        sx={{ cursor: "grabbing" }}
+        href="https://www.linkedin.com/in/rassielmex/"
+      >
+        <LinkedInIcon fontSize="large" />
+      </Link>
+      <Link
+        target={"_blank"}
+        rel="noopener"
+        color={"inherit"}
+        sx={{ cursor: "grabbing" }}
+        href="https://github.com/RassielMex"
+      >
+        <GitHubIcon fontSize="large" />
+      </Link>
+      <Link
+        target={"_blank"}
+        rel="noopener"
+        color={"inherit"}
+        sx={{ cursor: "grabbing" }}
+        href="https://wa.me/529991111524"
+      >
+        <WhatsAppIcon fontSize="large" />
+      </Link>
+    </Stack>
+  );
 };
 
 export default Contact;
