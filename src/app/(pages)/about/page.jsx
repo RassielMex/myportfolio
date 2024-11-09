dd7f98f7b318457bfb24216453d742d50aa5d0c0
RassielMex <rhcp94.rm@gmail.com>
2024-11-09 14:57:32 -0600

    Feat: about page

--- src/app/(pages)/about/page.jsx
+++ src/app/(pages)/about/page.jsx
@@ -1,68 +1,22 @@
-"use client";
-import React from "react";
-//import hello from "../../../public/images/hello.png";
-//import { motion } from "framer-motion";
+import { aboutInfo } from "../../seed/about-info";
+import { BookOpen, Book } from "@geist-ui/icons";
 
-const About = () => {
+export default function AboutPage() {
   return (
-    <div className="about-bg">
-      about
-      {/* <Container
-        sx={{
-          minHeight: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
-        <Stack alignItems={"center"}>
-          <motion.div
-            initial={{ opacity: 0.5 }}
-            animate={{
-              opacity: [0.75, 1, 1],
-              x: [-100, 100, 0],
-            }}
-            transition={{ duration: 2 }}
-          >
-            <img src={hello} alt="" className="about-img" />
-          </motion.div>
-        </Stack>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 2 }}
-        >
-          <Typography align="center" marginTop={"2rem"} variant="h4">
-            ¡Hola! Mi nombre es Rassiel Mex, soy Ingeniero Mecatrónico y
-            Desarrollador Web
-          </Typography>
-          <Typography align="center" marginTop={"2rem"} variant="h6">
-            Me fascina la programación y todo lo relacionado a la tecnología.{" "}
-            <br />
-            Aquí encontrarás algunos de mis proyectos más recientes.
-          </Typography>
-
-          <Stack alignItems={"center"} marginTop="2rem">
-            <Link
-              color={"inherit"}
-              target={"_blank"}
-              rel="noopener"
-              href="https://drive.google.com/file/d/1rwhkpDcYpUukwuTUvGebk8kpt0DeqG3A/view?usp=sharing"
-              underline="none"
-            >
-              <Button
-                variant="contained"
-                color="warning"
-                startIcon={<DownloadIcon />}
-              >
-                Descargar CV
-              </Button>
-            </Link>
-          </Stack>
-        </motion.div>
-      </Container> */}
+    <div>
+      <h1 className="text-5xl font-medium">Conoceme</h1>
+      <p className="text-base font-light">{aboutInfo.profile[0]}</p>
+      <h2>Education</h2>
+      <div>
+        <BookOpen size={24} />
+        <div>
+          <p>{aboutInfo.education[0].title}</p>
+          <p></p>
+        </div>
+      </div>
+      <h2>Courses</h2>
+      <BookOpen size={24} />
+      <h2>Languages</h2>
     </div>
   );
-};
-
-export default About;
+}
