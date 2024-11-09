399c4b55f96d8c1e1f8e879a4522e3a3f1b42cf7
RassielMex <rhcp94.rm@gmail.com>
2024-11-09 14:56:19 -0600

    Feat: text style

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased bg-black text-cyan-50`}>
+      <body
+        className={`${inter.className} antialiased bg-dark_2 text-light_90`}
+      >
         <div className="container mx-auto flex flex-col md:flex-row">
           <Sidebar />
           <div id="root" className="w-full md:w-3/4">
