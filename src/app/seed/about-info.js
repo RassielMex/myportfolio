f0767daf1fecbbdd44fbe8fcd924aaf755a55b6b
RassielMex <rhcp94.rm@gmail.com>
2024-11-09 14:56:46 -0600

    Feat: seed data

---
+++ src/app/seed/about-info.js
@@ -0,0 +1,58 @@
+export const aboutInfo = {
+  profile: [
+    `Ingeniero mecatrónico, con conocimientos en desarrollo web utilizando Javascript/Typescript y
+frameworks como Next/React. Experiencia usando tecnologias CSS como Tailwind, Material UI
+y Boostrap. Manejo de estado con React-Context, Redux-Toolkit y Zustand. Así como
+validaciones del lado del cliente usando Yup, Zod y Formik.`,
+    `Tengo conocimientos basicos de tecnologias backend como Node/Express y Nest.`,
+  ],
+  location: "Umán, Yucatan",
+  education: [
+    {
+      title: "Ingeniería en Mecatrónica",
+      place: "Universidad Autónoma de Yucatan",
+      location: "Mérida, Yucatán",
+      startDate: "Agosto 2012",
+      endDate: "Mayo 2017",
+    },
+  ],
+  courses: [
+    {
+      title: "React Js",
+      place: "Coderhouse",
+      duration: "Abril 2022 - Junio 2022",
+      certificate: "",
+    },
+    {
+      title: "React-The Complete Guide",
+      place: "UDEMY",
+      duration: "2022",
+    },
+    {
+      title: "Understanding Typescript",
+      place: "UDEMY",
+      duration: "2022",
+      certificate: "",
+    },
+    {
+      title: "CSS-The complete Guide",
+      place: "UDEMY",
+      duration: "2022",
+      certificate: "",
+    },
+    {
+      title: "FullStack Development",
+      place: "ITJ miCouch",
+      duration: "Mayo 2023",
+      certificate: "",
+    },
+  ],
+  languages: [
+    { title: "TOEFL ITP, 553", date: "FEB 2020", certificate: "" },
+    {
+      title: "EF SET, 71/100 C2 PROFICIENT",
+      date: "AUGUST 2022",
+      certificate: "https://www.efset.org/cert/CoNSBH",
+    },
+  ],
+};
